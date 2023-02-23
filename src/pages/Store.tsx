db07c862e1b987cf7ab66f069c7bba383c119d0d
NatalieIzotova <natallie79@gmail.com>
2023-02-23 09:24:06 -0800

    commit2

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import storeItems from "../data/items.json"
+import {Col, Row} from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+
 export  function Store (){
-    return <h1>Store</h1>
+    return(
+    <>
+    <h1>Tulip Store</h1>
+        <Row lg={3} md={2} xs={1} className="g-3">
+            {storeItems.map(item=>(
+                <Col key={item.id}>
+                    <StoreItem {...item}/>
+                </Col>
+            ))}
+        </Row>
+    </>
+)
 }
